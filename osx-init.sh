226de97b6b690e73f6ce25012d24bad038239452
Cameron Boehmer <cameron.boehmer@gmail.com>
2022-04-02 21:18:15 -0400

    updates

--- osx-init.sh
+++ osx-init.sh
@@ -1,28 +1,16 @@
 #! /bin/bash
 
-# New Mac Setup
-
-# Settings (currently manual)
-# - three-finger drag
-# - tap to click
-# - auto hide dock
-# - swap ctrl + caps
-# - option as meta in Terminal
-# - key repeat
-
-# Manual Things
-# - software
-#   - spectacles
-#   - simplenote
-#   - docker
-
+# TODO
+# - auto link
+#   - iterm2 keymap
+#   - rectangle settings
 
 # homebrew
 which brew 2> /dev/null
 if [[ $? != 0 ]]; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     echo "*****"
-    echo "Following the instructions for adding brew to your path and re-run $0"
+    echo "Follow the instructions for adding brew to your path, and then re-run $0"
     exit 0
 fi
 
@@ -34,6 +22,20 @@ brew install google-chrome
 brew install spotify
 brew install slack
 brew install iterm2
+brew install obsidian
+brew install rectangle
+
+# unix
+brew install emacs
+brew install coreutils
+brew services start emacs
+brew install pyenv
+brew install tree
+brew install gh
+brew install tmux
+brew install ag
+brew install telnet
+brew install entr
 
 # fish shell
 brew install fish fd bat fzf
@@ -41,12 +43,30 @@ curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 fisher install PatrickF1/fzf.fish
 echo "/opt/homebrew/bin/fish " >> /etc/shells
 chsh  -s /opt/homebrew/bin/fish
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 
-# other packages
-brew install emacs
-brew install coreutils
-brew services start emacs
-brew install gh
-brew install tmux
-brew install ag
-brew install telnet
+echo "
+Manual Things
+ Settings 
+ - three-finger drag
+ - tap to click
+ - shrink + auto hide dock
+ - swap ctrl + caps
+ - option as meta in iTerm
+ - key repeat fast
+ - key repeat delay short
+ - shortcuts
+   - disable spotlight
+ - finder
+   - show path bar
+
+ Apps
+ - docker
+ - rectangle prefs
+ - alfred snippets 
+ - iTerm2 keys
+" >> TODO
+
+echo "You have a TODO list in ./TODO"
+cat TODO
+
